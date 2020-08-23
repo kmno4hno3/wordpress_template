8bd387e745e690fef68e3f783ac9a03f0481cc48
kmno4hno3 <kmno4hno3@ezweb.ne.jp>
2020-08-23 19:02:50 +0900

    add_index.php_contents_roop

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <div class="container">
   <div class="contents">
- 
+    <?php if( have_posts()): while(have_posts()): the_post(); ?>
+      <!-- ここに記事一覧に表示したいコンテンツを記述 -->
+    <?php endwhile; endif;?> <!-- ループ終了 -->
   </div><!--end contents-->
   <?php get_sidebar(); ?>
 </div><!--end container-->
